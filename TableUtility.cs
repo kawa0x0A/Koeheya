58b75ea868e543da625939916d1f169be35ec960
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2022-03-20 13:50:28 +0900

    部屋編集ページにテーブルを追加

---
+++ TableUtility.cs
@@ -0,0 +1,50 @@
+﻿namespace Koeheya
+{
+    public static class TableUtility
+    {
+        public static string HeyaTableDataAttribute(Data.Heya[] heyas, int x, int y)
+        {
+            string classAttribute = "square";
+
+            if ((heyas == null) || (heyas.Length == 0))
+            {
+                return classAttribute;
+            }
+
+            var InnerWidthHeyas = heyas.Where(heya => (y >= heya.Y) && (y < (heya.Y + heya.Height)));
+            var InnerHeightHeyas = heyas.Where(heya => (x >= heya.X) && (x < (heya.X + heya.Width)));
+
+            if ((InnerWidthHeyas.Count() == 0) && (InnerHeightHeyas.Count() == 0))
+            {
+                return classAttribute;
+            }
+
+            if ((InnerWidthHeyas.Any(heya => ((heya.X <= x) && (x < (heya.X + heya.Width)))) && (InnerHeightHeyas.Any(heya => ((heya.Y <= y) && (y < (heya.Y + heya.Height)))))))
+            {
+                classAttribute += " heya";
+            }
+
+            if (InnerWidthHeyas.Any(heya => (x == heya.X)))
+            {
+                classAttribute += " left";
+            }
+
+            if (InnerWidthHeyas.Any(heya => ((x + 1) == (heya.X + heya.Width))))
+            {
+                classAttribute += " right";
+            }
+
+            if (InnerHeightHeyas.Any(heya => (y == heya.Y)))
+            {
+                classAttribute += " top";
+            }
+
+            if (InnerHeightHeyas.Any(heya => ((y + 1) == (heya.Y + heya.Height))))
+            {
+                classAttribute += " bottom";
+            }
+
+            return classAttribute;
+        }
+    }
+}
