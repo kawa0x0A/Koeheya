8d30737318948f716dad45b0c01c7df65f2de322
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2022-03-20 21:28:57 +0900

    部屋編集ページで部屋をドラッグアンドドロップで移動できる機能を追加

---
+++ wwwroot/js/EditHeya.js
@@ -0,0 +1,15 @@
+﻿export function getTableElementOffsetLeft() {
+    return window.document.getElementsByTagName('table')[0].offsetLeft;
+}
+
+export function getTableElementOffsetTop() {
+    return window.document.getElementsByTagName('table')[0].offsetTop;
+}
+
+export function getTableElementWidth() {
+    return window.document.getElementsByTagName('table')[0].clientWidth;
+}
+
+export function getTableElementHeight() {
+    return window.document.getElementsByTagName('table')[0].clientHeight;
+}
