18f77583ba64fc19b956e1068bfbb8c2bccca8ea
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2022-04-03 15:02:23 +0900

    部屋編集ページで部屋をドラッグアンドドロップでリサイズできる機能を追加

--- TableUtility.cs
+++ TableUtility.cs
@@ -1,10 +1,187 @@
 ﻿namespace Koeheya
 {
+    public enum HeyaStructEdgeType
+    {
+        Top,
+        Bottom,
+        Left,
+        Right,
+    }
+
+    public enum HeyaStructVertexType
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+    }
+
+    public enum HeyaStructVertexCrossType
+    {
+        None,
+        TopLeftTop,
+        TopLeftLeft,
+        TopLeftCross,
+        TopRightTop,
+        TopRightRight,
+        TopRightCross,
+        BottomLeftBottom,
+        BottomLeftLeft,
+        BottomLeftCross,
+        BottomRightBottom,
+        BottomRightRight,
+        BottomRightCross,
+    }
+
     public static class TableUtility
     {
+        public static bool ExistEdge(IEnumerable<Data.Heya> heyas, int x, int y, HeyaStructEdgeType edgeType)
+        {
+            var InnerWidthHeyas = heyas.Where(heya => (y >= heya.Y) && (y < (heya.Y + heya.Height)));
+            var InnerHeightHeyas = heyas.Where(heya => (x >= heya.X) && (x < (heya.X + heya.Width)));
+
+            switch (edgeType)
+            {
+                case HeyaStructEdgeType.Top:
+                    return InnerHeightHeyas.Any(heya => (y == heya.Y));
+
+                case HeyaStructEdgeType.Bottom:
+                    return InnerHeightHeyas.Any(heya => ((y + 1) == (heya.Y + heya.Height)));
+
+                case HeyaStructEdgeType.Left:
+                    return InnerWidthHeyas.Any(heya => (x == heya.X));
+
+                case HeyaStructEdgeType.Right:
+                    return InnerWidthHeyas.Any(heya => ((x + 1) == (heya.X + heya.Width)));
+            }
+
+            return false;
+        }
+
+        public static bool ExistVertex(IEnumerable<Data.Heya> heyas, int x, int y, HeyaStructVertexType vertexType)
+        {
+            switch (vertexType)
+            {
+                case HeyaStructVertexType.TopLeft:
+                    return (ExistEdge(heyas, x, y, HeyaStructEdgeType.Top) || ExistEdge(heyas, x, y, HeyaStructEdgeType.Left));
+                case HeyaStructVertexType.TopRight:
+                    return (ExistEdge(heyas, x, y, HeyaStructEdgeType.Top) || ExistEdge(heyas, x, y, HeyaStructEdgeType.Right));
+                case HeyaStructVertexType.BottomLeft:
+                    return (ExistEdge(heyas, x, y, HeyaStructEdgeType.Bottom) || ExistEdge(heyas, x, y, HeyaStructEdgeType.Left));
+                case HeyaStructVertexType.BottomRight:
+                    return (ExistEdge(heyas, x, y, HeyaStructEdgeType.Bottom) || ExistEdge(heyas, x, y, HeyaStructEdgeType.Right));
+            }
+
+            return false;
+        }
+
+        public static HeyaStructVertexCrossType GetVertexCrossType(IEnumerable<Data.Heya> heyas, int x, int y, HeyaStructVertexType vertexType)
+        {
+            switch (vertexType)
+            {
+                case HeyaStructVertexType.TopLeft:
+                    if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Top))
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Left))
+                        {
+                            return HeyaStructVertexCrossType.TopLeftCross;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.TopLeftTop;
+                        }
+                    }
+                    else
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Left))
+                        {
+                            return HeyaStructVertexCrossType.TopLeftLeft;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.None;
+                        }
+                    }
+
+                case HeyaStructVertexType.TopRight:
+                    if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Top))
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Right))
+                        {
+                            return HeyaStructVertexCrossType.TopRightCross;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.TopRightTop;
+                        }
+                    }
+                    else
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Right))
+                        {
+                            return HeyaStructVertexCrossType.TopRightRight;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.None;
+                        }
+                    }
+
+                case HeyaStructVertexType.BottomLeft:
+                    if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Bottom))
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Left))
+                        {
+                            return HeyaStructVertexCrossType.BottomLeftCross;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.BottomLeftBottom;
+                        }
+                    }
+                    else
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Left))
+                        {
+                            return HeyaStructVertexCrossType.BottomLeftLeft;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.None;
+                        }
+                    }
+
+                case HeyaStructVertexType.BottomRight:
+                    if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Bottom))
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Right))
+                        {
+                            return HeyaStructVertexCrossType.BottomRightCross;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.BottomRightBottom;
+                        }
+                    }
+                    else
+                    {
+                        if (ExistEdge(heyas, x, y, HeyaStructEdgeType.Right))
+                        {
+                            return HeyaStructVertexCrossType.BottomRightRight;
+                        }
+                        else
+                        {
+                            return HeyaStructVertexCrossType.None;
+                        }
+                    }
+            }
+
+            return HeyaStructVertexCrossType.None;
+        }
+
         public static string HeyaTableDataAttribute(IEnumerable<Data.Heya> heyas, int x, int y)
         {
-            string classAttribute = "square";
+            string classAttribute = "box";
 
             if ((heyas == null) || (heyas.Count() == 0))
             {
